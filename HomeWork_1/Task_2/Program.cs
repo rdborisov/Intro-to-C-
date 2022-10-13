f765b1b110ce690347c7e1954dda41ec44b05d32
Ruslan Borisov <cherdakruru@gmail.com>
2022-10-13 23:28:38 +0300

    Working on Task_2

--- HomeWork_1/Task_2/Program.cs
+++ HomeWork_1/Task_2/Program.cs
@@ -1,2 +1,2 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*Задача 2: Напишите программу, которая на вход принимает
+ два числа и выдаёт, какое число большее, а какое меньшее.*/
