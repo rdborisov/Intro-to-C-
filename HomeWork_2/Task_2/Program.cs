87d0a3f9ce8ad0fb1921c8bb7a4017b506e85c3a
Ruslan Borisov <cherdakruru@gmail.com>
2022-10-21 13:16:32 +0300

    Final Task_2, need modification

---
+++ HomeWork_2/Task_2/Program.cs
@@ -0,0 +1,24 @@
+﻿/*Задача 2: Напишите программу, которая выводит случайное трёхзначное число 
+и удаляет вторую цифру этого числа. Не использовать строки для расчета.
+456 -> 46
+782 -> 72
+918 -> 98
+*/
+Console.Clear();
+
+void Launch()
+{
+    int Num = new Random().Next(100, 1000);
+     if (Num / 100 > 0 && Num / 100 < 10)
+    {
+        int DigitOne = Num / 100;
+        int DigitTwo = Num % 10;
+        string NumIn = $"{DigitOne}{DigitTwo}"; // Реализовать более красиво!!!!
+        int NumOutTwoDigit = Convert.ToInt32(NumIn);
+
+        Console.WriteLine($"{Num} -> {NumOutTwoDigit}");
+    }
+    else Console.WriteLine("Число не 3-х значное");
+}
+
+Launch();
