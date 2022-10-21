b29a546c4af2eceb0927948c126e42534bb81a1d
Ruslan Borisov <cherdakruru@gmail.com>
2022-10-21 18:01:01 +0300

    Final Task_3

---
+++ HomeWork_2/Task_3/Program.cs
@@ -0,0 +1,34 @@
+﻿/*Задача 3: Напишите программу, которая выводит третью цифру заданного числа 
+или сообщает, что третьей цифры нет. Не использовать строки для расчета.
+645 -> 5
+78 -> третьей цифры нет
+326792 -> 6
+*/
+
+Console.Clear();
+
+int Prompt(string message)
+{
+    Console.Write(message);
+    return int.Parse(Console.ReadLine());
+}
+
+void Launch()
+{
+    
+    int Num = Prompt("Введите целое число: ");
+    int NumOne = Num;
+    if (Num < 100) Console.WriteLine($"{NumOne} -> Третьей цифры нет");
+    else 
+    {
+        while (Num > 1000)
+        {
+            Num = Num / 10;
+        }
+        Num = Num % 10;
+        Console.WriteLine($"{NumOne} -> {Num}");
+    }
+    
+}
+Launch();
+
