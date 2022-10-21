131cde82d50ce0984499fd6c0c78eaa84ca448bc
Ruslan Borisov <cherdakruru@gmail.com>
2022-10-21 23:16:43 +0300

    Final Task_3

--- HomeWork_3/Task_3/Program.cs
+++ HomeWork_3/Task_3/Program.cs
@@ -3,8 +3,26 @@
 3 -> 1, 8, 27
 5 -> 1, 8, 27, 64, 125
 */
-int Prompt(string msg)
-{
-    Console.Write(msg);
-    return int.Parse(Console.ReadLine());
+Console.Clear();
+
+double Launch()
+{   
+    int Prompt(string msg)
+    {
+        Console.Write(msg);
+        return int.Parse(Console.ReadLine());
+    }
+    
+    int Num = Prompt("Введите целое число: ");
+    double CubeNumber = Num;
+    Console.Write($"{Num} -> ");
+    for (int i = 1; i <= Num; i++)
+    {
+        CubeNumber = Math.Pow(i, 3);
+        if (i < Num) Console.Write($"{CubeNumber}, ");
+        else Console.Write($"{CubeNumber}");
+    }
+    return CubeNumber;
 }
+    
+Launch();
