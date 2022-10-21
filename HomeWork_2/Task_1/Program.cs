98cf462efe11331580b4ff5ccd5558ff26e4b0e7
Ruslan Borisov <cherdakruru@gmail.com>
2022-10-21 12:31:27 +0300

    Final Task_1

---
+++ HomeWork_2/Task_1/Program.cs
@@ -0,0 +1,26 @@
+﻿/*Задача 1: Напишите программу, которая принимает на вход трёхзначное число 
+и на выходе показывает вторую цифру этого числа. Не использовать строки для расчета.
+456 -> 5
+782 -> 8
+918 -> 1
+*/
+Console.Clear();
+
+int Prompt(string mesage)
+{
+    Console.Write(mesage);
+    return int.Parse(Console.ReadLine()); 
+}
+
+void TwoDigit()
+{
+    int Num = Prompt("Введите 3-х значное число: ");
+    if (Num / 100 > 0 && Num / 100 < 10)
+    {
+        int TwoDigit = Num / 10 % 10;
+        Console.WriteLine($"{Num} -> {TwoDigit}");
+    }
+    else Console.WriteLine("Число не трёхзначное");
+}
+
+TwoDigit();
