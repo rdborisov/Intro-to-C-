c5b542dfe045936c5ff445da2cc5d204f9acd89f
Ruslan Borisov <cherdakruru@gmail.com>
2022-10-21 21:56:14 +0300

    Normalisation Task_2

--- HomeWork_3/Task_2/Program.cs
+++ HomeWork_3/Task_2/Program.cs
@@ -5,23 +5,31 @@ A (7,-5, 0); B (1,-1,9) -> 11.53
 */
 Console.Clear();
 
-int Prompt(string msg)
-{
-    Console.WriteLine(msg);
-    return int.Parse(Console.ReadLine());
-}
-
 int[] dotA = new int[3];
 int[] dotB = new int[3];
 
 void FillArray(int[] dotA, int[] dotB)
 {
+    int Prompt(string msg)
+    {
+        Console.WriteLine(msg);
+        return int.Parse(Console.ReadLine());
+    }
+
     for (int i = 0; i < dotA.Length; i++)
     {
         dotA[i] = Prompt("Введите координату точкиA: ");
         dotB[i] = Prompt("Введите координату точкиB: ");
-        /*dotA[i] = new Random().Next(-10, 10);
-        dotB[i] = new Random().Next(-10, 10);*/
+    }
+}
+
+void PrintArr(int[] ArrA)
+{
+    for (int i = 0; i < ArrA.Length; i++)
+    {
+        
+        if (i == ArrA.Length - 1) Console.Write($"{ArrA[i]}");
+        else Console.Write($"{ArrA[i]}, ");
     }
 }
 
@@ -33,14 +41,16 @@ void Mathematic(int[] ArrA, int[] ArrB)
     {   
         double PowDiff = Math.Pow((ArrA[i] - ArrB[i]), 2);
         Sum += PowDiff;
-        Console.WriteLine($"{PowDiff} : {Sum}");
     }
     Dist = Math.Sqrt(Sum);
-    Console.WriteLine($"Dist_3D: {(int)Dist}");
+    Console.Write($"Dist: {Dist:f2}");
 }
-FillArray(dotA, dotB);
 
-Console.WriteLine();
-PrintArray(dotB);
-Console.WriteLine();
+FillArray(dotA, dotB);
+Console.Write("A(");
+PrintArr(dotA);
+Console.Write(") ");
+Console.Write("B(");
+PrintArr(dotB);
+Console.Write(") -> ");
 Mathematic(dotA, dotB);
