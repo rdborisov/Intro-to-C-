f626d3ac4741034061b6f9cb311dc5a84d51440d
Ruslan Borisov <rdborisov99@gmail.com>
2022-11-10 10:12:44 +0300

    Final H4_T2

---
+++ HomeWork_4/Task_2/Program.cs
@@ -0,0 +1,38 @@
+﻿/*Задача 2: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+452 -> 11
+82 -> 10
+9012 -> 12
+*/
+class H4T2
+{
+
+    public static void Main()
+    {
+        int Input(string message)
+        {
+            Console.Write(message);
+            return int.Parse(Console.ReadLine());
+        }
+
+        int SumNum(int Num)
+        {
+            int Sum = 0;
+            while(Num > 0) 
+            {
+                int lastDigit = Num % 10;
+                Sum += lastDigit;
+                Num /= 10;
+            }
+            return Sum;
+        }
+
+        void Output()
+        {
+            Console.Clear();
+            int Num = Input("Input Num: ");
+            Console.WriteLine($"{Num} -> {SumNum(Num)}");
+        }
+        
+        Output();
+    }
+}
