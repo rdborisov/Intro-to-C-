1f92ee095855e61aa47622439b1f5c65779f1f16
Ruslan Borisov <rdborisov99@gmail.com>
2022-11-18 14:35:39 +0300

    Final H7T1

--- HomeWork_7/Task_1/Program.cs
+++ HomeWork_7/Task_1/Program.cs
@@ -7,3 +7,46 @@ m = 3, n = 4.
 8 7,8 -7,1 9
 */
 
+class H7T1
+{
+    public static void Main()
+    {
+        int UserInput(string message)
+        {
+            Console.Write(message);
+            return int.Parse(Console.ReadLine());
+        }
+
+        double[,] CreateMatrix(int m, int n, int max, int min)
+        {
+            double[,] Matrix = new double[m, n];
+            for (int i = 0; i < Matrix.GetLength(0); i++)
+            {
+                for (int j = 0; j < Matrix.GetLength(1); j++)
+                {
+                    Matrix[i, j] = new Random().NextDouble() * (max - min) + min;
+                }
+            }
+            return Matrix;
+        }
+
+        void PrintMatrix(double[,] Matrix)
+        {
+            for (int i = 0; i < Matrix.GetLength(0); i++)
+            {
+                for (int j = 0; j < Matrix.GetLength(1); j++)
+                {
+                    Console.Write($"{Matrix[i, j]:f1} ");
+                }
+                Console.WriteLine();
+            }
+        }
+
+        void Output()
+        {
+            double[,] Matrix = CreateMatrix(UserInput("m := "), UserInput("n := "), -5, 10);
+            PrintMatrix(Matrix);
+        }
+        Output();
+    }
+}
