75c5055314657c805f7f616a802448444f6312f9
Ruslan Borisov <rdborisov99@gmail.com>
2022-11-23 23:28:23 +0300

    Final H8T4

--- HomeWork_8/Task4/Program.cs
+++ HomeWork_8/Task4/Program.cs
@@ -5,3 +5,48 @@
 11 16 15 06
 10 09 08 07
 */
+
+namespace HomeWork8
+{
+    class Task4
+    {
+        public static void Main()
+        {
+            int size = 4;
+            SpiralOutput(size);
+        }
+
+        public static void PrintMatrix(int[,] matrix)
+        {
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                for (int j = 0; j < matrix.GetLength(1); j++)
+                {
+                    if (matrix[i, j] < 10) Console.Write($" {matrix[i, j]} ");
+                    else Console.Write($"{matrix[i, j]} ");
+                }
+                Console.WriteLine();
+            }
+        }
+
+        public static void SpiralOutput(int size)
+        {
+            
+            int value = 1;
+            int i = 0, j = 0;
+            int[,] matrix = new int[size, size];
+            
+            while (size != 0)
+            {
+                int k = 0;
+                do { matrix[i, j++] = value++; } while (++k < size - 1);
+                for (k = 0; k < size - 1; k++) matrix[i++, j] = value++;
+                for (k = 0; k < size - 1; k++) matrix[i, j--] = value++;
+                for (k = 0; k < size - 1; k++) matrix[i--, j] = value++;
+
+                ++i; ++j; size = size < 2 ? 0 : size - 2;
+            }
+            PrintMatrix(matrix);
+        }
+    }
+}
