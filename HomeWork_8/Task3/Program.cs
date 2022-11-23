0969500ae91a7bb781cd976e1b5278167ef5eb7c
Ruslan Borisov <rdborisov99@gmail.com>
2022-11-23 16:59:24 +0300

    Final H8T3

--- HomeWork_8/Task3/Program.cs
+++ HomeWork_8/Task3/Program.cs
@@ -6,3 +6,98 @@
 18 20
 15 18
 */
+using System.Diagnostics;
+
+namespace HomeWork8
+{
+    class Task3
+    {
+        public static void Main()
+        {
+            Console.Clear();
+            int[,] matrixA = CreateMatrix(3, 3);
+            int[,] matrixB = CreateMatrix(3, 3);
+            PrintMatrix(matrixA);
+            Console.WriteLine("---");
+            PrintMatrix(matrixB);
+            Console.WriteLine("---");
+
+            if (VerificationMultiplication(matrixA, matrixB))
+            {
+                int[,] result = MultiplicationMatrix(matrixA, matrixB);
+                PrintMatrix(result);
+            }
+            else Console.WriteLine($"Произведение матриц невозможно, матрицы несогласованы");
+
+        }
+
+        public static int[,] CreateMatrix(int rows, int collums)
+        {
+            int[,] matrix = new int[rows, collums];
+
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                for (int j = 0; j < matrix.GetLength(1); j++)
+                {
+                    matrix[i, j] = new Random().Next(0, 10);
+                }
+            }
+            return matrix;
+        }
+
+        public static void PrintMatrix(int[,] matrix)
+        {
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                for (int j = 0; j < matrix.GetLength(1); j++)
+                {
+                    Console.Write($"{matrix[i, j]} ");
+                }
+                Console.WriteLine();
+            }
+        }
+
+        public static bool VerificationMultiplication(int[,] matrixA, int[,] matrixB)
+        {
+            return (matrixA.GetLength(1) == matrixB.GetLength(0)) ? true : false;
+        }
+
+        public static int[,] MultiplicationMatrix(int[,] matrixA, int[,] matrixB)
+        {
+            if (matrixA.GetLength(0) > matrixB.GetLength(1))
+            {
+                int[,] result = new int[matrixA.GetLength(0), matrixB.GetLength(1)];
+
+                for (int i = 0; i < matrixA.GetLength(0); i++)
+                {
+                    for (int j = 0; j < matrixB.GetLength(1); j++)
+                    {
+
+                        for (int k = 0; k < result.GetLength(0); k++)
+                        {
+                            result[i, j] += matrixA[i, k] * matrixB[k, j];
+                        }
+                    }
+                }
+                return result;
+            }
+            else 
+            {
+                int[,] result = new int[matrixA.GetLength(1), matrixB.GetLength(0)];
+
+                for (int i = 0; i < matrixA.GetLength(0); i++)
+                {
+                    for (int j = 0; j < matrixB.GetLength(1); j++)
+                    {
+
+                        for (int k = 0; k < result.GetLength(0); k++)
+                        {
+                            result[i, j] += matrixA[i, k] * matrixB[k, j];
+                        }
+                    }
+                }
+                return result;
+            }
+        }
+    }
+}
